d34abb8e7952e3e65c0953416c826d857de315be
dingpq <dingpq@si-tech@com.cn>
2026-06-09 16:45:53 +0800

    Remove chinaMap3d game module

--- kids-game-simple/src/games/chinaMap3d/index.ts
+++
@@ -1,9 +0,0 @@
-import { ChinaMap3DGame } from './game';
-
-export function initChinaMap3D(container: HTMLDivElement): ChinaMap3DGame {
-  return new ChinaMap3DGame(container);
-}
-
-export { ChinaMap3DGame } from './game';
-export { GAME_CONFIG, QUESTIONS, COLORS } from './config';
-export { loadRecords, resetRecords } from './logic/storage';
